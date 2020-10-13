f66c13ed24f8eefa79b296745e3e66a8caa59bcb
Mehdi A Noorani <noorani.mehdi@outlook.com>
2020-10-13 00:16:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ require "pry"
 
 def nyc_pigeon_organizer(data)
   
-end_results = data.each_with_object(({}) do |(key, value), final_array|
+end_results = data.each_with_object({}) do |(key, value), final_array|
   
   value.each do |inner_key, names|
     names.each do |name|
